67298f1cf7630398ad3fd44d904f335702a09f6f
Dave Stanton <davestanton@Daves-MacBook-Pro.local>
2022-11-11 16:08:43 -0500

    Basic image and text.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+
+#wrapper {
+  background: url(./mythicalchili.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color: #fff;
+  height: 1200px;
+}
+#wrapper h1 {
+    padding-top: 32px;
+    text-align: center;
+}
+#wrapper p {
+    margin: 0 20%;
+    font-size: 18px;
+    line-height: 27px;
+}
+</style>
+
+<div id="wrapper">
+    <h1>Mythical Chili</h1>
+    <p>Chili is in our DNA. The Mythical Freedom Chili Brigade champions chili excellence and is committed to making the world better through outreach and a good bowl of chili. We support all chiliheads, regardless of bean or pepper preferences, and we recognize the value that our various perspectives bring to the culinary arena.</p>
+    <p>The Mythical Freedom Chili Brigade’s chili recipes were forged in the heart of a dying star. We use only the best and most mythical ingredients to set your soul on fire and your mouth ablaze.</p>
+</div>
