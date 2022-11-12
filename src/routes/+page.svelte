a2ae26f60f68043c6bafd7dc26cab6e84eeed0e4
Dave Stanton <davestanton@Daves-MacBook-Pro.local>
2022-11-12 09:55:35 -0500

    Link to ICS voting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,13 @@
     padding-top: 32px;
     text-align: center;
 }
+
+#wrapper h2 {
+    text-align: center;
+}
+#wrapper h2 a {
+    color: #fff;
+}
 #wrapper p {
     margin: 0 20%;
     font-size: 18px;
@@ -25,6 +32,10 @@
 
 <div id="wrapper">
     <h1>Mythical Chili</h1>
+    <h2>
+        <a href="https://ipswebdesign.wufoo.com/forms/chili-tasting-2022-at-moosehaven">
+                Click here to vote</a> for us (team 17) as the International Chili Society's People's Champion 
+    </h2>
     <p>Chili is in our DNA. The Mythical Freedom Chili Brigade champions chili excellence and is committed to making the world better through outreach and a good bowl of chili. We support all chiliheads, regardless of bean or pepper preferences, and we recognize the value that our various perspectives bring to the culinary arena.</p>
     <p>The Mythical Freedom Chili Brigade’s chili recipes were forged in the heart of a dying star. We use only the best and most mythical ingredients to set your soul on fire and your mouth ablaze.</p>
 </div>
